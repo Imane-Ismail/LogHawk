b3d0a34eeea8bd9111e7940902f65bbf57938e8f
Imane-Ismail <86661348+Imane-Ismail@users.noreply.github.com>
2025-02-22 22:50:43 +1100

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,11 +1,13 @@
-# loghawk/cli.py
 import pyfiglet
+import colorama
+from colorama import Fore, Style
 
 def main():
+    colorama.init()  # Initialize colorama for Windows support
 
-    # Generate the ASCII art text for 'LogHawk'
+    # Generate fancy ASCII text
     ascii_art = pyfiglet.figlet_format("LogHawk")
-
-    # Print the fancy text to the console
-    print(ascii_art)
-    print("Your Log Analysis Tool")
+    
+    # Print colored text
+    print(Fore.CYAN + ascii_art + Style.RESET_ALL)
+    print(Fore.GREEN + "Your Log Analysis Tool\n" + Style.RESET_ALL)
